7ecf6a3eab4851ff03314619558207d7988ef769
Rik Meijer <nostriphant@rikmeijer.nl>
2026-06-30 16:23:23 +0200

    simplify client

--- src/Client.php
+++ src/Client.php
@@ -3,21 +3,21 @@
 namespace nostriphant\Client;
 
 readonly class Client {
-    
+
     private \Closure $log;
-    
+
     private array $connections;
-    
+
     private array $speakers;
     private array $listeners;
-    
+
     public function __construct(callable $log, \Amp\Websocket\Client\WebsocketConnection ...$connections) {
         $this->log = \Closure::fromCallable($log);
         $this->connections = $connections;
         $this->speakers = array_map(fn($connection) => new Speech($connection), $this->connections);
         $this->listeners = array_map(fn($connection) => new Hearing($connection), $this->connections);
     }
-    
+
     public static function connectToUrl(callable $log, string ...$urls) {
         $connections = [];
         $attempts = 0;
@@ -42,32 +42,32 @@ readonly class Client {
         }
         return new self($log, ...$connections);
     }
-    
+
     public function __invoke(callable $speak_callback) : void {
         $subscriptions = new \nostriphant\Functional\Index();
         $events = new \nostriphant\Functional\Index();
-        
+
         $speak = function(\nostriphant\NIP01\Message $message) {
             return \nostriphant\Functional\Iterator::walk($this->speakers, fn(Speech $speak) => \Amp\async(fn() => $speak($message)));
         };
-        
-        
+
+
         $event = function(\nostriphant\NIP01\Event $event, callable $reply) use ($speak, $events) {
             $events[$event->id] = $reply;
             return $speak(\nostriphant\NIP01\Message::event($event));
         };
-        
-        
-        $futureSpeaking = \Amp\async($speak_callback, $event, new SubscriptionFactory($speak, $subscriptions));
-        $futureListening = \Amp\async(fn() => \nostriphant\Functional\Iterator::walk($this->listeners, fn(Hearing $listener) => $listener(fn(\nostriphant\NIP01\Message $message, callable $stop) => match($message->type) {
-            'EVENT' => $subscriptions[$message->payload[0]](new \nostriphant\NIP01\Event(...$message->payload[1]), $stop),
-            'OK' => $events[$message->payload[0]]($message->payload[1], $message->payload[2], $stop),
-            'EOSE' => $subscriptions[$message->payload[0]](null, $stop),
-            default => ($this->log)($message)
-        })));
-        
-        \Amp\Future\await([$futureSpeaking, $futureListening], new \Amp\SignalCancellation([SIGINT, SIGTERM]));
-        
+
+
+        $speak_callback($event, new SubscriptionFactory($speak, $subscriptions));
+        $futureListening = \Amp\async(\Closure::fromCallable([\nostriphant\Functional\Iterator::class, 'walk']), $this->listeners, fn(Hearing $listener) => $listener(fn(\nostriphant\NIP01\Message $message, callable $stop) => match ($message->type) {
+                            'EVENT' => $subscriptions[$message->payload[0]](new \nostriphant\NIP01\Event(...$message->payload[1]), $stop),
+                            'OK' => $events[$message->payload[0]]($message->payload[1], $message->payload[2], $stop),
+                            'EOSE' => $subscriptions[$message->payload[0]](null, $stop),
+                            default => ($this->log)($message)
+                        }));
+
+        \Amp\Future\await([$futureListening], new \Amp\SignalCancellation([SIGINT, SIGTERM]));
+
         \nostriphant\Functional\Iterator::walk($this->connections, fn(\Amp\Websocket\Client\WebsocketConnection $connection, int $index) => $connection->close());
     }
 }
